5c9bdebae14e5bf942263f7df2851e3698469182
Technoblade <william@williamgiles.co.nz>
2024-09-03 11:23:19 +1200

    review popup

--- src/components/account/AccountReviewListItem.tsx
+++ src/components/account/AccountReviewListItem.tsx
@@ -17,6 +17,14 @@ import {
 import { useState } from "react";
 
 import StarRating from "../Review/StarRating";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "~/components/ui/dialog";
 import {
   AlertDialog,
   AlertDialogAction,
@@ -42,6 +50,7 @@ export default function AccountReviewListItem({
   const deleteReviewMutation = api.review.deleteReview.useMutation();
 
   const [deleteDialogOpen, setDeleteDialogOpen] = useState(false);
+  const [reviewDialogOpen, setReviewDialogOpen] = useState(false);
 
   const deleteBook = async () => {
     deleteReviewMutation.mutate(
@@ -62,6 +71,13 @@ export default function AccountReviewListItem({
       },
     );
   };
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const publicationDateString =
+    review.book.publicationDate instanceof Date
+      ? // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+        review.book.publicationDate.toDateString()
+      : "Unknown";
 
   return (
     <>
@@ -93,7 +109,9 @@ export default function AccountReviewListItem({
                 <DropdownMenuItem asChild>
                   <Link href={`/book/${review.book.id}`}>View Book</Link>
                 </DropdownMenuItem>
-                <DropdownMenuItem>View Review</DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setReviewDialogOpen(true)}>
+                  View Review
+                </DropdownMenuItem>
               </DropdownMenuGroup>
 
               <DropdownMenuSeparator />
@@ -126,6 +144,49 @@ export default function AccountReviewListItem({
           </AlertDialogFooter>
         </AlertDialogContent>
       </AlertDialog>
+
+      <Dialog open={reviewDialogOpen} onOpenChange={setReviewDialogOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogDescription>
+              <div className="flex flex-col gap-6 md:flex-row">
+                <div className="w-32 md:w-1/3 mx-auto">
+                  <Image
+                    src={review.book.b64Image ?? ""}
+                    alt="Book cover"
+                    width={300}
+                    height={400}
+                    className="rounded-lg object-cover shadow-md"
+                  />
+                </div>
+                <div className="w-full md:w-2/3">
+                  <div className="mb-2 text-3xl font-bold text-white">
+                    {review.book.title}
+                  </div>
+
+                  <div className="mb-2 flex items-center justify-center md:justify-start">
+                    <StarRating rating={review.rating} />
+                  </div>
+                  <p className="mb-4 text-gray-400">{review.book.author}</p>
+                  <p className="font-semibold text-gray-200">Comment:</p>
+                  <p className="mb-6 text-gray-100">
+                    {review.comment}
+                  </p>
+     
+                </div>
+                <div className="w-full md:w-2/3">
+                <Button variant={"destructive"} onClick={() => {
+                  setReviewDialogOpen(false);
+                  setDeleteDialogOpen(true);
+                }}>
+                  Delete Review
+                </Button>
+                </div>
+              </div>
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </>
   );
 }
